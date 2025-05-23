be0f97eea82d1963d75eb6cf10a4e714cf61bdc4
nickkupriyan0v <nickkupriyanov@gmail.com>
2025-05-23 23:34:36 +0300

    fixed all cities

--- all_cities.py
+++ all_cities.py
@@ -1,5 +1,9 @@
+from pathlib import Path
+
 def get_all_cities() -> list[str]:
     cities = []
-    with open('cities.txt', 'r', encoding='utf-8') as file:
+    script_dir = Path(__file__).parent  # Директория скрипта
+    file_path = script_dir / 'cities.txt'
+    with open(file_path, 'r', encoding='utf-8') as file:
         cities = [line.strip().lower() for line in file if line.strip()]
     return cities
